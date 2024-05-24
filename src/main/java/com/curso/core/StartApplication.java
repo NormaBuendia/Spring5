4db83307391887a0483ffd3169d12d8b2ea8be43
Norma Buendia <93407023+NormaBuendia@users.noreply.github.com>
2024-05-23 22:10:43 -0400

    Update StartApplication.java

--- src/main/java/com/curso/core/StartApplication.java
+++ src/main/java/com/curso/core/StartApplication.java
@@ -1,13 +1,12 @@
-package com.curso.core;
+package com.mkyong.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StartApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(StartApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
-
 }
